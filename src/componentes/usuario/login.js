2bc01f60db3be1fcdf959c5b615f726d906a687d
Pipe <andresfelipeacevedovergara@gmail.com>
2022-11-04 02:03:54 -0500

    Login

--- src/componentes/usuario/login.js
+++ src/componentes/usuario/login.js
@@ -2,40 +2,34 @@ import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-import '../../styles/registro.css'
+import '../../styles/login.css'
 
 export class Login extends Component {
   render() {
     return (
         <div className="form-signin text-center w-100 m-auto">
           <Form>
-            <img className="mb-3 logo" 
-                  src="https://cdn1.iconfinder.com/data/icons/zeon-medical-i-2/32/pharmacy_shop_drug_medicine-256.png" 
-                  alt="logo" />
+            
     
-            <h1 className="h3 mb-3 fw-normal">Inicio de sesiónn</h1>
+            <h1 className="h3 mb-3 fw-normal">Inicio de sesión</h1>
     
             <Form.Group className="form-floating">
-              <Form.Control type="text" id="floatingName" placeholder="placeholder" className="form-control" />
-              <label>Nombre</label>
+              
             </Form.Group>
-            <Form.Group className="form-floating">
-              <Form.Control type="text" id="floatingLastName" placeholder="placeholder" className="form-control" />
-              <label>Apellido</label>
-            </Form.Group>
+      
             <Form.Group className="form-floating">
               <Form.Control type="email" id="floatingEmail" placeholder="placeholder" className="form-control" />
-              <label>Correo</label>
+              <label>Usuario</label>
             </Form.Group>
             <Form.Group className="form-floating">
               <Form.Control type="password" id="floatingPassword" placeholder="placeholder" className="form-control" />
               <label>Contraseña</label>
             </Form.Group>
                 
-            <p>¿Ya estás registrado? <a href="/">Inicia sesión</a></p>
+            <p>¿Olvidó su contraseña? <a href="/">Restablecer</a></p>
     
             <div className="d-grid">
-              <Button type="submit" className="w-100 btn btn-lg btn-success">Registrarse</Button>
+              <Button type="submit" className="w-100 btn btn-lg btn-success">Entrar</Button>
             </div>
           </Form>
         </div>
